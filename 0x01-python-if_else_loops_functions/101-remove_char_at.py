6ecdc7a78d9323fa02a93e73c2ce0efe5c7ceb7b
codemayor19 <codemayor19@gmail.com>
2023-11-29 13:49:46 -0800

    new commit

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    copy = ""
+    i = 0
+    while(i < len(str)):
+        if i == n:
+            i += 1
+        else:
+            copy += str[i]
+            i += 1
+    return copy
