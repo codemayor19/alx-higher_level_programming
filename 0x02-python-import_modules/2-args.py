6e8cfe9f47c3444fed7cdf47d5c041176c1206bb
codemayor19 <codemayor19@gmail.com>
2023-11-30 05:36:29 -0800

    new commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) > 1:
+    if len(sys.argv) - 1 > 1:
         print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
+    elif len(sys.argv) - 1 == 0:
+        print("{} arguments.".format(0))
     else:
-        print("{}".format("0 arguments."))
+        print("{} arguments:".format(len(sys.argv) - 1))
+        print("{}: {}".format(len(sys.argv) - 1, sys.argv[1]))
