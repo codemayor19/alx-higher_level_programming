b8d424883b8253b1aa893448511e2347d29bfeac
codemayor19 <codemayor19@gmail.com>
2023-11-30 12:03:10 -0800

    new commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,20 +4,24 @@ def calculate():
     from calculator_1 import add, sub, mul, div
     arg = sys.argv
     size = len(sys.argv) - 1
+    n1 = int(arg[1])
+    n2 = int(arg[3])
+    op = arg[2]
     if size != 3:
         print("Usage: {} <a> <operator> <b>".format(arg[0]))
         sys.exit(1)
-    elif arg[2] == '+':
-        print("{} {} {} = {}".format(arg[1], arg[2], arg[3], add(int(arg[1]), int(arg[3]))))
-    elif arg[2] == '-':
-        print("{} {} {} = {}".format(arg[1], arg[2], arg[3], sub(int(arg[1]), int(arg[3]))))
-    elif arg[2] == '*':
-        print("{} {} {} = {}".format(arg[1], arg[2], arg[3], mul(int(arg[1]), int(arg[3]))))
-    elif arg[2] == '/':
-        print("{} {} {} = {}".format(arg[1], arg[2], arg[3], div(int(arg[1]), int(arg[3]))))
+    elif op == '+':
+        print("{} {} {} = {}".format(n1, op, n2, add(n1, n2)))
+    elif op == '-':
+        print("{} {} {} = {}".format(n1, op, n2, sub(n1, n2)))
+    elif op == '*':
+        print("{} {} {} = {}".format(n1, op, n2, mul(n1, n2)))
+    elif op == '/':
+        print("{} {} {} = {}".format(n1, op, n2, div(n1, n2)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     calculate()
