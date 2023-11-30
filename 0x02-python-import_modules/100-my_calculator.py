cb2a14d226384a4156c75493afd431f2241e9289
codemayor19 <codemayor19@gmail.com>
2023-11-30 12:06:31 -0800

    new commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,13 +4,13 @@ def calculate():
     from calculator_1 import add, sub, mul, div
     arg = sys.argv
     size = len(sys.argv) - 1
-    n1 = int(arg[1])
-    n2 = int(arg[3])
-    op = arg[2]
     if size != 3:
         print("Usage: {} <a> <operator> <b>".format(arg[0]))
         sys.exit(1)
-    elif op == '+':
+    n1 = int(arg[1])
+    n2 = int(arg[3])
+    op = arg[2]
+    if op == '+':
         print("{} {} {} = {}".format(n1, op, n2, add(n1, n2)))
     elif op == '-':
         print("{} {} {} = {}".format(n1, op, n2, sub(n1, n2)))
