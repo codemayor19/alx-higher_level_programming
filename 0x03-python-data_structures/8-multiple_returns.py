7ed581494c2f5b629ff0a4cb9721afe16f97ab8a
codemayor19 <codemayor19@gmail.com>
2023-12-01 04:54:07 -0800

    new commit

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        return (tuple([len(sentence), None]))
+    else:
+        return (tuple([len(sentence), sentence[0]]))
