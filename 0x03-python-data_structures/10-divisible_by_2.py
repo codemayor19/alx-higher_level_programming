4480862fb5b2e90507e2abb2c5c8528bc75777dd
codemayor19 <codemayor19@gmail.com>
2023-12-03 05:13:00 -0800

    new commit

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     _blist = my_list[:]
     for count, i in enumerate(my_list):
         if i % 2 == 0:
-            _blist[count] == True
+            _blist[count] = True
         else:
             _blist[count] = False
     return (_blist)
